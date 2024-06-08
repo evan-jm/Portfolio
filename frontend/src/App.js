bc37fe5179ec238e26597019c6ecfed5407b0522
Evan Mitchell <ejm0214@gmail.com>
2024-06-07 22:56:47 -0400

    Updating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import "./App.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.js";
 import Navbar from "./components/Navbar/Navbar.js";
 
@@ -7,11 +6,7 @@ function App() {
   return (
     <div className="app">
       <Navbar />
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </div>
   );
 }
