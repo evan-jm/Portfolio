15dade7b2b99cf67f9f9dbc9372f5c945109b267
Evan Mitchell <ejm0214@gmail.com>
2024-06-30 12:22:56 -0400

    Updating styling

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <Link to="/Portfolio">Home</Link>
-      <Link to="/Portfolio/about">About</Link>
-      <Link to="/Portfolio/projects">Projects</Link>
-      <Link to="/Portfolio/experience">Experience</Link>
-      <Link to="/Portfolio/education">Education</Link>
-      <Link to="/Portfolio/contact">Contact Me</Link>
-    </nav>
+    <div className="nav-container">
+      <nav className="navbar">
+        <Link to="/Portfolio">Home</Link>
+        <Link to="/Portfolio/about">About</Link>
+        <Link to="/Portfolio/projects">Projects</Link>
+        <Link to="/Portfolio/experience">Experience</Link>
+        <Link to="/Portfolio/education">Education</Link>
+        <Link to="/Portfolio/contact">Contact Me</Link>
+      </nav>
+    </div>
   );
 };
 
--- frontend/src/pages/Education/Education.js
+++ frontend/src/pages/Education/Education.js
@@ -3,12 +3,12 @@ import "./Education.scss";
 const Education = () => {
   return (
     <div className="education" id="education">
-      <center>
-        <h2>Education</h2>
-      </center>
-      <h4>Bachelor of Science in Computer Science</h4>
-      <p>Stockton University</p>
-      <p>Graduated Cum Laude in December 2023 (GPA: 3.81)</p>
+      <h2>Education</h2>
+      <div className="contents">
+        <h4>Bachelor of Science in Computer Science</h4>
+        <p>Stockton University</p>
+        <p>Graduated Cum Laude in December 2023 (GPA: 3.81)</p>
+      </div>
     </div>
   );
 };
